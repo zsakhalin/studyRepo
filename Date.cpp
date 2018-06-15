e8a7a48d0e772ab9954d6c16a0e2b4d05f73705e
Alex Z <androidmania001@gmail.com>
2018-06-15 19:46:06 +0300

    class Date + dynamically alloc memory

--- Date.cpp
+++ Date.cpp
@@ -5,21 +5,59 @@ using namespace std;
 class Date
 {
 public:
-	void Mew(){cout << "Mew" << endl;}
-// 	Date();
-// 	~Date();
+	Date();
+	~Date();
+	void Mew();
 	
-// private:
+private:
 	// int dmy[8];
-	int dmy;
+	int* ptr_to_alloc = new int[size];
 };
-
+Date::Date(int size){}
+Date::~Date(){}
+void Date::Mew()
+{
+	cout << "Mew" << endl;
+}
 int main()
 {
 	cout << "hello " << endl;
 	Date a1;
 	a1.Mew();
-	a1.dmy = 1;
+	// a1.dmy = 1;
 	cout << a1.dmy;
 	return 0;
 }
+
+//Выделяем динамически память, храним указатель на область и сопутствующие данные.
+// int main()
+// {
+//     const size_t size = 5;
+//     int* arr = new int[size];
+ 
+//     for (int i = 0; i < size; ++i) 
+//         arr[i] = i; 
+ 
+//     int* tmp_a = arr;
+ 
+//     const size_t size2 = size * 2;
+ 
+//     arr = nullptr;
+//     arr = new int[size2];
+ 
+//     for (int i = 0; i < size2; ++i)
+//     {
+//         if (i < size)
+//             arr[i] = tmp_a[i];
+//         else
+//             arr[i] = i;
+//     }
+ 
+//     for (int i = 0; i < size2; ++i)
+//         std::cout << arr[i] << std::endl;
+ 
+//     delete[] tmp_a;
+//     delete[] arr;
+ 
+//     return 0;
+// }
