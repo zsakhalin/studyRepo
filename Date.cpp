14e136f1820a708e02effbe6200ff91288e487f1
sanYong <androidmania001@gmail.com>
2018-06-16 17:47:48 +0300

    added a.out to ignore. added ptr_to_alloc member

--- Date.cpp
+++ Date.cpp
@@ -5,59 +5,47 @@ using namespace std;
 class Date
 {
 public:
-	Date();
+	Date(int dd, int mm, int yyyy);
+	Date(const Date &);
 	~Date();
-	void Mew();
+	// void SetDD(int dd){ ptr_to_alloc[0] = dd;}
+	int GetDD() const{ return *ptr_to_alloc;}
 	
 private:
-	// int dmy[8];
-	int* ptr_to_alloc = new int[size];
+	int size = 3;
+	int* ptr_to_alloc;
+
 };
-Date::Date(int size){}
-Date::~Date(){}
-void Date::Mew()
+Date::Date(int dd, int mm, int yyyy)
 {
-	cout << "Mew" << endl;
+	ptr_to_alloc =  new int[3];
+	cout << "Constructor called" << endl;
+	ptr_to_alloc[0] = dd;
+	ptr_to_alloc[1] = mm;
+	ptr_to_alloc[2] = yyyy;
+	cout << "\t" << ptr_to_alloc[0] << "." << ptr_to_alloc[1] << "." << ptr_to_alloc[2] << "." << endl;
+}
+Date::Date(const Date & theDate)
+{
+	cout << "CopyConstructor called" << endl;
+	// ptr_to_alloc = new int[size];
+
+	ptr_to_alloc = new int[3];
+	*ptr_to_alloc = *(theDate.ptr_to_alloc);
+	cout << "\t" << ptr_to_alloc[0] << "." << ptr_to_alloc[1] << "." << ptr_to_alloc[2] << "." << endl;
+}
+Date::~Date()
+{
+	cout << "Destructor called" << endl;
+	delete[] ptr_to_alloc;
 }
 int main()
 {
-	cout << "hello " << endl;
-	Date a1;
-	a1.Mew();
-	// a1.dmy = 1;
-	cout << a1.dmy;
+	cout << "________ " << endl;
+	Date s(10,2,3);
+	Date t(s);
+	Date t1(10,12,300) ;
+	t = t1;
+	cout << t.GetDD() << endl;
 	return 0;
 }
-
-//Выделяем динамически память, храним указатель на область и сопутствующие данные.
-// int main()
-// {
-//     const size_t size = 5;
-//     int* arr = new int[size];
- 
-//     for (int i = 0; i < size; ++i) 
-//         arr[i] = i; 
- 
-//     int* tmp_a = arr;
- 
-//     const size_t size2 = size * 2;
- 
-//     arr = nullptr;
-//     arr = new int[size2];
- 
-//     for (int i = 0; i < size2; ++i)
-//     {
-//         if (i < size)
-//             arr[i] = tmp_a[i];
-//         else
-//             arr[i] = i;
-//     }
- 
-//     for (int i = 0; i < size2; ++i)
-//         std::cout << arr[i] << std::endl;
- 
-//     delete[] tmp_a;
-//     delete[] arr;
- 
-//     return 0;
-// }
